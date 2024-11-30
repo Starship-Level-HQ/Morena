47b8778a7c82232cd3d3b8c7669311116ecbfeea
Mikhail <evdokimm05@gmail.com>
2024-11-30 12:49:43 +0700

    update dialog

--- dialog.lua
+++ dialog.lua
@@ -1,30 +1,45 @@
 Dialog = {
-  new = function(bodies, phrases, order, callback)
+  new = function(phrases, callback)
     local self = {}
     self.bodies = bodies
     self.phrases = phrases
-    self.order = order
     self.callback = callback
     self.current = 0
 
     function self.draw(d1, d2, d3, d4)
       if self.current > 0 then
         love.graphics.setColor(1, 1, 1, 0.5)
-        love.graphics.rectangle("fill", bodies[order[self.current]]:getX() - 23, bodies[order[self.current]]:getY() - 90, 220, 31)
+        love.graphics.rectangle("fill", phrases[self.current].body:getX() - 23, phrases[self.current].body:getY() - 90, 14*#phrases[self.current].text, 31)
         love.graphics.setColor(0, 0, 0, 1)
-        love.graphics.print(phrases[self.current], bodies[order[self.current]]:getX() - 23, 
-          bodies[order[self.current]]:getY() - 90, 0, 2, 2)
+        love.graphics.print(phrases[self.current].text, phrases[self.current].body:getX() - 23, 
+          phrases[self.current].body:getY() - 90, 0, 2, 2)
         love.graphics.setColor(d1, d2, d3, d4)
       end
     end
 
-    function self.update()
-      self.current = self.current + 1
-      if self.current > #self.phrases then
-        self.callback()
-        return bodies[1]
+    function self.update(dt)
+      if self.dt == nil then
+        self.dt = dt
+        self.current = self.current + 1
+        if phrases[self.current].dur == nil then
+          phrases[self.current].dur = 0.9
+        end
+        
+      else 
+        self.dt = self.dt + dt
+        if phrases[self.current].dur == nil then
+          phrases[self.current].dur = 0.9
+        end
+        if self.dt >= phrases[self.current].dur then
+          self.dt = 0
+          self.current = self.current + 1
+          if self.current > #self.phrases then
+            self.callback()
+            return nil
+          end
+        end
       end
-      return bodies[order[self.current]]
+      return phrases[self.current].body
     end
 
     return self
