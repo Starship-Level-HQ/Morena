84bf36a4d359e53a99c171f126e520885e4b9a7c
Mikhail <evdokimm05@gmail.com>
2024-12-07 09:42:34 +0700

    ground items don't stop characters

--- inventory/src/objectsOnMap.lua
+++ inventory/src/objectsOnMap.lua
@@ -11,28 +11,27 @@ MapStaff = {
         self.nextId = 0
         function self:addItem(x, y, id)
             local newItem = ItemModule.create_item(id)
+            self:newItem(x, y, newItem)
+        end
+        
+        function self:newItem(x, y, item)
+            local newItem = item
             local angle = math.random() * math.pi  -- Случайный угол
             local itemBody = {x = x, y = y, item = newItem, angle = angle, collision = false, id = self.nextId}
-            itemBody.body = love.physics.newBody(world, x, y, "static")
+            itemBody.body = love.physics.newBody(world, x, y, "kinematic")
+            itemBody.body:setGravityScale(0)
             itemBody.shape = love.physics.newRectangleShape(24, 24)
             itemBody.fixture = love.physics.newFixture(itemBody.body, itemBody.shape)
             itemBody.fixture:setCategory(cat.ITEM)
+            itemBody.fixture:setMask(cat.VOID, cat.ENEMY, cat.P_SHOT, cat.E_SHOT, cat.DASHING_PLAYER, cat.NPC)
             itemBody.fixture:setUserData(itemBody)
+            itemBody.fixture:setSensor(true)
             table.insert(self.items, itemBody)
             self.nextId = self.nextId + 1
         end
 
         function self:dropItem(x, y, item)
-            local newItem = item
-            local angle = math.random() * math.pi  -- Случайный угол
-            local itemBody = {x = x, y = y, item = newItem, angle = angle, collision = false, id = self.nextId}
-            itemBody.body = love.physics.newBody(world, x, y, "static")
-            itemBody.shape = love.physics.newRectangleShape(24, 24)
-            itemBody.fixture = love.physics.newFixture(itemBody.body, itemBody.shape)
-            itemBody.fixture:setCategory(cat.ITEM)
-            itemBody.fixture:setUserData(itemBody)
-            table.insert(self.items, itemBody)
-            self.nextId = self.nextId + 1
+            self:newItem(x, y, item)
         end
 
         function self:takeItemByID(itemID)
@@ -53,7 +52,7 @@ MapStaff = {
                 else
                     love.graphics.setColor(d1, d2, d3, d4)
                 end
-                love.graphics.draw(itemData.item.img, itemData.x, itemData.y, itemData.angle, 0.5, 0.5, 12, 19)
+                love.graphics.draw(itemData.item.img, itemData.x, itemData.y, itemData.angle, 0.7, 0.7, 12, 19)
             end
         end
         
