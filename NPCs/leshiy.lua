29a05d3cc7ca4cc03de8734a2117cbfca90bfe66
Mikhail <evdokimm05@gmail.com>
2025-01-19 21:43:44 +0700

    dead sprite

--- NPCs/leshiy.lua
+++ NPCs/leshiy.lua
@@ -7,18 +7,18 @@ Leshiy = {
     self.spriteSheet = love.graphics.newImage('res/sprites/leshiy2.png')
     self.grid = anim8.newGrid(43, 82, self.spriteSheet:getWidth(), self.spriteSheet:getHeight())
     self.animations = {}
-    self.animations.down = anim8.newAnimation(self.grid('1-4', 1), 0.2)
-    self.animations.up = anim8.newAnimation(self.grid('1-4', 2), 0.2)
-    self.animations.right = anim8.newAnimation(self.grid('1-4', 1), 0.2)
-    self.animations.left = anim8.newAnimation(self.grid('1-4', 1), 0.2)
+    self.animations.down = anim8.newAnimation(self.grid('1-4', 1), 0.3)
+    self.animations.up = anim8.newAnimation(self.grid('1-4', 2), 0.3)
+    self.animations.right = anim8.newAnimation(self.grid('1-4', 1), 0.3)
+    self.animations.left = anim8.newAnimation(self.grid('1-4', 1), 0.3)
     self.zoom = 3
     
     self.deadSpriteSheet = love.graphics.newImage('res/sprites/leshiy-dead.png')
-    self.deadGrid = anim8.newGrid(46, 82, self.deadSpriteSheet:getWidth(), self.deadSpriteSheet:getHeight())
+    self.deadGrid = anim8.newGrid(28, 30, self.deadSpriteSheet:getWidth(), self.deadSpriteSheet:getHeight())
     if not userConfig.blood then
-      self.deadAnimations = anim8.newAnimation(self.deadGrid('1-1', 1), 1)
+      self.deadAnimations = anim8.newAnimation(self.deadGrid('1-1', 2), 1)
     else
-      self.deadAnimations = anim8.newAnimation(self.deadGrid('2-2', 1), 1)
+      self.deadAnimations = anim8.newAnimation(self.deadGrid('1-1', 1), 1)
     end
     return self
   end
